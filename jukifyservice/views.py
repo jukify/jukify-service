106ae46bd088d480b6ce26601b9d4e95b4632b2c
fmachado091 <fmachado091@gmail.com>
2017-10-22 15:21:02 -0200

    Add configs

--- jukifyservice/views.py
+++ jukifyservice/views.py
@@ -5,9 +5,19 @@ from rest_framework.parsers import JSONParser
 from jukifyservice.models import User
 from jukifyservice.serializers import UserSerializer
 
+import configparser
+
+# import client tokens from configs file
+configs = configparser.ConfigParser()
+configs.read('jukifyservice/configs.ini')
+MY_CLIENT_ID = configs['jukify']['client_id']
+MY_CLIENT_SECRET = configs['jukify']['client_secret']
+MY_REDIRECT_URI = configs['jukify']['redirect_uri']
+
+
 def get_code(request, code):
     """
     Just a proof of concept
     """
     if request.method == 'GET':
-        return JsonResponse({'foo': code})
+        return JsonResponse({'code': code})
