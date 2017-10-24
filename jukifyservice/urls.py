70c5e8158e06aea6f47c2d4398fa77b75933401b
fmachado091 <fmachado091@gmail.com>
2017-10-24 00:40:58 -0200

    Create group and add user

--- jukifyservice/urls.py
+++ jukifyservice/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from jukifyservice import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^login/', views.login),
-    url(r'^user/all/', views.list_users),
+    url(r'^admin', admin.site.urls),
+    url(r'^login', views.login),
+    url(r'^user', include([
+        url(r'all', views.list_users),
+    ])),
+    url(r'^group', include([
+        url(r'^$', views.create_group),
+        url(r'(?P<group_id>[0-9]+)/add$', views.add_user_to_group),
+    ]))
 ]
